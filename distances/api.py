1b1a814c8197e5dd774e4935a613e83c65d5d14a
Patrik Beck <patrik.beck@gmail.com>
2017-07-07 10:45:37 +0200

    removed unused imports

--- distances/api.py
+++ distances/api.py
@@ -1,35 +1,9 @@
-from xml.etree.ElementTree import fromstring, XMLParser, TreeBuilder
 
-import sys
-
-from datetime import timedelta
 from django.http import JsonResponse
-from django.urls import reverse
 from django.views.decorators.http import require_http_methods
-from django.db import transaction, IntegrityError
-
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
-from django.utils import timezone
-
-from django.contrib.auth import authenticate, login as auth_login, update_session_auth_hash
 
 import json
-import requests
-from rollbar import report_exc_info
-
-from email_campaigns.models import Campaign, EmailTemplate
-from quoting import google_api
-from quoting.models import json_user, Product, Animal, Quote, PRODUCT_LIFETIME, Address
-from livestock.models import SubCategory, Category
-from one_click_auth.models import Token
-
-from email_campaigns.utils import schedule_send_email
-
-from xmljson import badgerfish as bf
-
-from django.conf import settings
-import time
 
 from distances.matrix import get_distance
 
